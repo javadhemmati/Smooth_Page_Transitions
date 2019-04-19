336602802af6f6d939b8ba6bd022181de8670946
javadhemmati <javadhemmati192@gmail.com>
2019-04-20 02:43:44 +0430

    Beautiful transition between html pages
    Build menu and three pages
    Adding style to pages
    Create a transition to navigate between pages with "@ dogstudio / highway" and "gsap" in javascript

---
+++ js/transition.js
@@ -0,0 +1,16 @@
+import Highway from "@dogstudio/highway";
+import { TimelineLite } from "gsap";
+
+class Fade extends Highway.Transition {
+  in({ from, to, done }) {
+    const tl = new TimelineLite();
+    tl.fromTo(to, 0.5, { left: "-100%", top: "10vh" }, { left: "0%" })
+    .fromTo(to, 0.5, { height: "5px" }, { height: "90vh", top: "10%", onComplete: function() { from.remove(); done(); } })
+    .fromTo(to.children[0], 2, {opacity: 0}, {opacity: 1});
+  }
+  out({ from, done }) {
+    done();
+  }
+}
+
+export default Fade;
